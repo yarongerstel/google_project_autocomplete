acf744351c1499e709e947939bb25973f495cd01
arielhaser <59341179+arielhaser@users.noreply.github.com>
2021-08-09 10:31:02 +0300

    new commit 2

--- venv/init.py
+++ venv/init.py
@@ -2,8 +2,7 @@ import pandas as pd
 import os
 
 # Folder Path
-#path = r'2021-archive'
-path = r'test'
+path = r'2021-archive'
 
 # Read text File
 def read_text_file(file_path, input):
@@ -14,21 +13,19 @@ def read_text_file(file_path, input):
         os.makedirs(new_dir)
     read_file.to_csv(f"fixed/{file_path[:-3]}csv")
     df = pd.read_csv(f"fixed/{file_path[:-3]}csv")
-    lst = list(df.loc[df['line content'].str.contains(input), 'line content'])
+    lst = list(df.loc[df['line content'].str.contains(input, na=False), 'line content'])
     if len(lst) != 0:
-        print(lst[0])
+        print(lst[0]+" "+file_path)
 
 
 def prepend_line(file_name, line):
     """ Insert given string as a new line at the beginning of a file """
     # define name of temporary dummy file
     # open original file in read mode and dummy file in write mode
-    with open(file_name, 'r') as read_obj:
+    with open(file_name, 'r', encoding='utf8') as read_obj:
         temp = read_obj.readlines()
-    with open(file_name, 'w') as write_obj:
-        # Write given line to the dummy file
+    with open(file_name, 'w', encoding='utf8') as write_obj:
         write_obj.write(line + '\n')
-        # Read lines from original file one by one and append them to the dummy file
         for selected_line in temp:
             write_obj.write(selected_line)
 
