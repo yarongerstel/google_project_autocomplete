67b5522deaa001547e45d93dee46bc406280311c
arielhaser <59341179+arielhaser@users.noreply.github.com>
2021-08-10 09:18:51 +0300

    almost final commit

--- venv/init.py
+++ venv/init.py
@@ -7,6 +7,20 @@ path = r'2021-archive'
 pool = (
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
     'w', 'x', 'y', 'z')
+main_lst = []
+
+class AutoCompleteData:
+    def __init__(self, cs, st, o,s):
+        self.completed_sentence = cs
+        self.source_text = st
+        self.offset = o
+        self.score = s
+
+    completed_sentence: str
+    source_text: str
+    offset: int
+    score: int
+
 
 
 # Read text File
@@ -16,30 +30,58 @@ def read_text_file(file_path, input):
     if not os.path.isdir(new_dir):
         os.makedirs(new_dir)
     read_file.to_csv(f"fixed/{file_path[:-3]}csv")
-    df = pd.read_csv(f"fixed/{file_path[:-3]}csv")
-    lst = list(df.loc[df['line content'].str.contains(input, na=False), 'line content'])
+
+
+def searchIt(file_path, input):
+    df = pd.read_csv(file_path,low_memory=False)
+    lst = list(df.loc[df['line'].str.contains(input, na=False), 'line'])
     if len(lst) == 0:
         for i in full_combinations(input):
-            lst = list(df.loc[df['line content'].str.contains(i[0], na=False), 'line content'])
+            lst = list(df.loc[df['line'].str.contains(i[0], na=False), 'line'])
             if len(lst) != 0:
                 for item in lst:
-                    print(item + " score: " + str(len(input) * 2 - int(i[1])) + " location: " + file_path)
+                    instance = AutoCompleteData(item,file_path,0, len(input) * 2 - int(i[1]) )
+                    main_lst.appeand(instance)
+                    #print(item + " score: " + str(len(input) * 2 - int(i[1])) + " location: " + file_path)
                 break
     else:
         for item in lst:
-            print(item + " score: " + str(len(input) * 2) + " location: " + file_path)
+            instance = AutoCompleteData(item, file_path, 0, len(input) * 2)
+            main_lst.append(instance)
+            #print(item + " score: " + str(len(input) * 2) + " location: " + file_path)
+
+def mySort(instance):
+    return instance.score
+
+def finish_search():
+    main_lst.sort(key=mySort)
+    for item in main_lst:
+        print(item.completed_sentence)
+        print(item.source_text)
+        print(item.offset)
+        print(item.score)
+        print('\n')
+
+def superFastSearch(path, input):
+    for filename in os.listdir(path):
+        f = os.path.join(path, filename)
+        # checking if it is a file
+        if os.path.isfile(f):
+            searchIt(f, input)
+        else:
+            superFastSearch(f, input)
 
 
 def full_combinations(statement):
     for letter in pool:  # switch
         for i in range(len(statement)):
-            temp = statement[:i] + letter + statement[i:]
+            temp = statement[:i] + letter + statement[1+i:]
             score_down = calculate_char(statement, temp) + 2
             yield temp, score_down
     for letter in pool:  # add
         for i in range(len(statement)):
-            temp = statement[:i] + letter + statement[i + 1:]
-            score_down = calculate_char(statement, temp) * 2 + 2  # get down also for worng char
+            temp = statement[:i] + letter + statement[i:]
+            score_down = calculate_char(statement, temp) * 2  # get down also for worng char
             yield temp, score_down
     for tup in itertools.combinations(statement, len(statement) - 1):  # sub
         temp = ''.join(tup)
@@ -56,7 +98,7 @@ def calculate_char(original, copy):
     if len(original) >= 5 and len(copy) >= 5:
         return 1
     min_num = min(len(original), len(copy))
-    return min_num
+    return 5 - min_num
 
 
 def prepend_line(file_name, line):
@@ -83,13 +125,24 @@ def checkOver(path, input):
 
 
 def startup(input):
-    checkOver(path, input)
+    #checkOver(path, input)
+    superFastSearch('fixed/'+path, input)
 
 
 def main():
     a = input("enter statement: ")
     startup(a)
-
+    finish_search()
+    """temp=""
+    a = input("enter statement: ")
+    while(a!='#'):
+        a.lower().strip()
+        #agnore multy space
+        while "  " in a:
+            a=a.replace("  "," ")
+        temp = temp + a
+        startup(temp)
+        a = input(temp)"""
 
 if __name__ == '__main__':
     main()
